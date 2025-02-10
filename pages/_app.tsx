ca14f865f8100166ad7b91f6b2ba8a5a0a04376c
zhugechina <zhugetd@126.com>
2025-02-10 23:38:12 +0800

    rm gtag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,12 +12,10 @@ export default function App({ Component, pageProps }: AppProps): ReactElement {
   return (<>
     <Head>
       <meta name="saashub-verification" content="zm6rcf35ll2n" />
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4907197973761221"
-        crossOrigin="anonymous"></script>
     </Head >
 
     {/* Google tag (gtag.js)  */}
-    <Script async src="https://www.googletagmanager.com/gtag/js?id=G-54K88LRQBS"
+    {/* <Script async src="https://www.googletagmanager.com/gtag/js?id=G-54K88LRQBS"
       strategy='afterInteractive'>
     </Script>
     <Script>
@@ -28,7 +26,7 @@ export default function App({ Component, pageProps }: AppProps): ReactElement {
 
         gtag('config', 'G-54K88LRQBS');
       `}
-    </Script>
+    </Script> */}
 
     <Component {...pageProps} />
   </>)
